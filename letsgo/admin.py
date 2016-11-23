16675ba684ca859c95374c2f8ff6bff750c1a80e
Ozan Dost ŞAHİN <odost.sahin@tedu.edu.tr>
2016-11-24 01:03:35 +0200

    Update admin.py
    added filtering options

--- letsgo/admin.py
+++ letsgo/admin.py
@@ -5,28 +5,13 @@ from datetime import datetime, timedelta
 
 
 
-def show_last_day(modelAdmin, request, queryset):
-	
-	Places.objects.filter(time=datetime.today())
-
-show_last_day.short_description="Show checkins from last day"
-
-def show_last_week(modelAdmin, request, queryset):
-
-	last_week=(datetime.today() - timedelta(days=7))
-	Places.objects.filter(time__lt=last_week)
-
-show_last_week.short_description="Show checkins form last week"
-#def show_last_month(modelAdmin, request, queryset):
-
 @admin.register(Places)
 class VenueAdmin(admin.ModelAdmin):
 	list_display = ('time','place_id','formatted_time', 'name', 'checkins', 'herenow', 'category')
 	#ordering = ['category']
 	actions = [show_last_day,show_last_week]
+	list_filter = ('category','time',)
 
-@admin.register(Cafe)
-class CafeAdmin(admin.ModelAdmin):
-	list_display = ('time','place_id', 'name', 'herenow', 'checkins')
+
 
 
