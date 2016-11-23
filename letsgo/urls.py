1848789015cd182dc32a8f4904a3d944489ee285
Ozan Dost ŞAHİN <odost.sahin@tedu.edu.tr>
2016-11-24 01:12:33 +0200

    Update urls.py

--- letsgo/urls.py
+++ letsgo/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 urlpatterns=[
 	url(r'^$', 'letsgo.views.index'),
-	url(r'^$', 'letsgo.views.cafe'),
 	#url(r'^feature/$', 'letsgo.views.make_featured', name='feature'),
 	#url(r'^count_likes/$', 'letsgo.views.count_like', name='count_likes'),
 ]
